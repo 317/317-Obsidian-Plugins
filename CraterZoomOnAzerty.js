2256c83418145c2ca159155b7e37da7b50f49a95
Thierno <t.rignoux@gmail.com>
2020-10-11 08:32:58 +0200

    Update CraterZoomOnAzerty.js
    Add new hotkey

--- CraterZoomOnAzerty.js
+++ CraterZoomOnAzerty.js
@@ -36,7 +36,7 @@ class CraterZoomOnAzertyPlugin {
       callback: () => this.resetZoom()
     });
 
-    this.app.hotkeyManager.customKeys["bzoom:zoomOut"] = [{ modifiers: ["Mod"], key: "-" }];
+    this.app.hotkeyManager.customKeys["bzoom:zoomOut"] = [{ modifiers: ["Mod"], key: "-" }, { modifiers: ["Mod"], key: "ù" }]];
     this.app.hotkeyManager.customKeys["bzoom:zoomIn"] = [{ modifiers: ["Mod"], key: "=" }, { modifiers: ["Mod"], key: "+" }];
     this.app.hotkeyManager.customKeys["bzoom:reset"] = [{ modifiers: ["Mod"], key: "à" }, { modifiers: ["Mod"], key: "0" }];
   }
